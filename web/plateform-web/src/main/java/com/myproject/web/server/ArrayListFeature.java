fad623762c6cb5ecbb40482e9316a698704c9ebf
liweilin <1105468795@qq.com>
2018-07-23 16:09:13 +0800

    ArrayList的remove方法

--- web/plateform-web/src/main/java/com/myproject/web/server/ArrayListFeature.java
+++ web/plateform-web/src/main/java/com/myproject/web/server/ArrayListFeature.java
@@ -13,6 +13,7 @@ public class ArrayListFeature {
         Person person1 = new Person("name");
 
         personList1.add(person1);
+        personList1.remove(0);
 
         System.out.println("personList1.size() = " + personList1.size());
         System.out.println("personList2.size() = " + personList2.size());
@@ -57,7 +58,50 @@ public class ArrayListFeature {
 //                newCapacity = hugeCapacity(minCapacity);
 //            elementData = Arrays.copyOf(elementData, newCapacity);
 //      }
-
-
+//      数组一旦在堆内存中创建，数组的长度就是不变的如果想要删除数组中的元素，
+//      只能把想要删除的相应位置的值置换为null,(数组空洞)，容易造成空指针异常。把null后面值复制一份，
+//      向前拷贝，但最后的一位的引用的指向是不变的，与倒数第二位指向同一对象。再次处理，将最后一位重
+//      置为null,但数组的长度还是10需要自定义一个size来记录数组长度。
+//      ArrayList的元素删除方式
+//      按照下标删除
+//        List<Person> perList = new ArrayList<>();
+//        perList.add(new Person("name0"));
+//        perList.add(new Person("name1"));
+//        perList.add(new Person("name3"));
+//        perList.add(new Person("name4"));
+//
+//        perList.add(1, new Person("name5"));
+//        perList.remove(1);
+//        perList.remove(new Person("name4"));
+//        System.out.println(perList.size());
+//      remove方法的源码
+//        public E remove(int index) {
+//            rangeCheck(index); // 检查数组是否越界
+//            modCount++; // 记录修改次数
+//            E oldValue = elementData(index);
+//            int numMoved = size - index - 1; // 计算需要拷贝的元素个数
+//            if (numMoved > 0 //将index + 1 之后的元素向前拷贝
+//                System.arraycopy(elementData, index + 1, elementData, index, numMoved);
+//            elementData[--size] = null;  size = size - 1， elementData[size] = null; size减少1，数组最后一位赋值为null
+//        }
+//      直接删除元素
+//        public boolean remove(Object o) {
+//            if (o == null) {  // 循环寻找null值
+//                for (int index = 0; index < size; index++){
+//                    if (elementData[index] == null){
+//                      fastRemove(index); // 找到null值执行fastRemove
+//                      return true;
+//                    }
+//                }
+//            } else {
+//                 for (int index = 0; index < size; index ++){
+//                     if (o.equals(elementData[index]) {
+//                          fastRemove(index);
+//                          return true;
+//                     }
+//                 }
+//            }
+//            return false;
+//        }
     }
-}
+    }
